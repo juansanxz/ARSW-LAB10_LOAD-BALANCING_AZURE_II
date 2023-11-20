11a1f8c787a6ad4f1490a7025b8ad35153fe6e95
Juan Sánchez <juan.sanchez-pe@mail.escuelaing.edu.co>
2023-11-19 21:02:40 -0500

    Función Fibnacci recursiva

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,27 +1,42 @@
 var bigInt = require("big-integer");
+let memo = {};
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let nth = req.body.nth
-    let nth_1 = bigInt.one;
-    let nth_2 = bigInt.zero;
-    let answer = bigInt.zero;
+    let nth = req.body.nth; // Parseamos el valor de entrada a entero
+  
 
-    if (nth < 0)
-        throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
-        }
+    if (nth < 0) {
+        throw 'must be greater than or equal to 0'; // Validación para valores negativos
     }
 
-    context.res = {
-        body: answer.toString()
-    };
+    // Función recursiva con memoization para calcular Fibonacci
+    function fibonacciMemo(n) {
+        if (n in memo) {
+            context.log("ya existe");
+            return memo[n]; // Si el resultado ya está calculado, lo devuelve directamente
+        }
+        if (n === 0) {
+            return bigInt.zero;
+        }
+        if (n <= 2) {
+            return bigInt.one; // Los primeros dos números de Fibonacci son 1
+        }
+
+        memo[n] = fibonacciMemo(n - 1).add(fibonacciMemo(n - 2));
+        return memo[n];
+    }
+
+  
+
+    try {
+        let answer = fibonacciMemo(nth);
+
+        context.res = {
+            body: answer.toString()
+        };
+    } catch (error) {
+        context.log.error('Ocurrió un error:', error);
+    }
+    
 }
